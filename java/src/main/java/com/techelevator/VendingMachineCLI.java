8130614cf84d164e374e820c871e38f4756fbfc6
Nicholas Scacchetti <nscacchetti@gmail.com>
2022-11-17 11:01:33 -0500

    Nick outline update

--- java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,10 +2,8 @@ package com.techelevator;
 
 import com.techelevator.application.VendingMachine;
 
-public class VendingMachineCLI 
-{
-    public static void main(String[] args) 
-    {
+public class VendingMachineCLI {
+    public static void main(String[] args) {
         VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.run();
     }
@@ -16,7 +14,25 @@ public class VendingMachineCLI
             slot no, name of product, price, quantity, BOGODO (price - 1)
             initial quantity = 6
             add item counter
+       Audit file./ writer method:
+       start writer at main beginning
 
+        void writerMethod(Writer object, String action/item, double transaction amount in dollars, remainingMoney) {
+
+        date = getcurrent date
+        time = get current time
+        action = action/item
+        transaction string = $ transaction amount
+        balance = remianing money
+
+        String to wrtire = date+ " "+ time+ " " .........
+        use writer to wrtier to file
+
+     }
+
+
+             use filewriter(or whatever doesnt overwrite the file like printwriter) to append entry to audit.txt
+             get current date and time
 
         Display Method:
             if (odd)
@@ -26,7 +42,9 @@ public class VendingMachineCLI
             if discountApplies, then apply BOGODO prices
         Purchase:
             (M) Feed Money: 1, 5, 10, 20
-                While more money is left to be added
+                While more money is left to be added to remainingMoney
+                writerMethod
+
             (S) Select Item:
                 Use display method
                 if quantity is 0 display: "NO LONGER AVAILABLE."
@@ -41,9 +59,36 @@ public class VendingMachineCLI
                         if drink: "Drinky, Drinky, Slurp Slurp!"
                         if gum: "Chewy, Chewy, Lots O Bubbles!"
                     itemCounter++
+                    writerMethod
                     run purchase method
 
             (F) Finish Transaction:
+                    remainingMoney = 3.35
+
+                    changeAfterDollars = remainingMoney % 1 = 0.35
+                    dollarCount= (remainingMoney - changeAfterDollars)/1 = (3.35-.35)/1 = 3
+
+                    remainingMoney = 0.35
+                    changeAfterDollars = remainingMoney % 0.25 = 0.10
+                    quarterCount= (remainingMoney - changeAfterDollars)/0.25 = (0.35-.25)/0.25 = 1
+
+                      remainingMoney = 0.10
+                    changeAfterDollars = remainingMoney % 0.10 = 0
+                    dimeCount= (remainingMoney - changeAfterDollars)/0.10 = (0.10-0)/0.1 = 1
+
+                      remainingMoney = 0.0
+                    changeAfterDollars = remainingMoney % 0.05 = 0
+                    nickelCount= (remainingMoney - changeAfterDollars)/0.05 = (0.0-0)/0.05 = 0
+
+                    fedMoney= 0
+                  print dollars, quarter, dimes, nickels
+                  writerMethod
+                    run main to return
+
+
+
+
+
                     remaining money % 1
                         remainingMoney = remainingMoney - remaining money % 1
                     remaining money % 0.25
@@ -53,5 +98,5 @@ public class VendingMachineCLI
                     remaining money % 0.05
                         remainingMoney = remainingMoney - remaining money % 0.05
      */
-    
+
 }
